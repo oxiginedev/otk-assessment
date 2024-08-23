8199d9353c407959690aa00a30a6273b0b531423
Adedaramola Adetimehin <mailstokindle@gmail.com>
2024-08-23 12:12:49 +0100

    chore: fix success message design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
         
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 @session('success')
-                    <div class="mb-4 p-4 border border-green-300 bg-green-50 font-medium text-sm text-green-600">
+                    <div class="mb-4 p-4 border border-green-300 bg-green-50 font-medium text-sm text-green-600 rounded-md">
                         {{ $value }}
                     </div>
                 @endsession
